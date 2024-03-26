b4d8a7e208348c5da5cd8b9ea918beb36eee9dff
Elizabeth Shiu <eshiu2@uwo.ca>
2024-03-26 11:39:53 -0400

    info component

---
+++ src/infoForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+
+
+
+const InfoForm = () => {
+
+    return(
+        <>
+            <h1>Info</h1>
+            <Input placeholder="Basic usage" />
+        </>
+    )
+}
+
+export default InfoForm;
